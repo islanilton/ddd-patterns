a46a253275027efa55593e834f6f3f8921c35218
Islanilton <islanilton.rodrigues@gmail.com>
2023-11-01 12:47:09 -0300

    chore: create the product service

---
+++ src/service/product.service.spec.ts
@@ -0,0 +1,22 @@
+import { Product } from '@/entity/product'
+import { ProductService } from '@/service/product.service'
+
+describe('Product service unit test', () => {
+  it('should be able change the prices of all products', () => {
+    const product1 = new Product({
+      id: '1',
+      name: 'Product 1',
+      price: 100,
+    })
+    const product2 = new Product({
+      id: '2',
+      name: 'Product 2',
+      price: 200,
+    })
+    const products = [product1, product2]
+    ProductService.increasePrice(products, 100)
+
+    expect(product1.price).toBe(200)
+    expect(product2.price).toBe(400)
+  })
+})
---
+++ src/service/product.service.ts
@@ -0,0 +1,9 @@
+import { Product } from '@/entity/product'
+
+export class ProductService {
+  static increasePrice(products: Product[], percentage: number): void {
+    for (const product of products) {
+      product.changePrice((product.price * percentage) / 100 + product.price)
+    }
+  }
+}
