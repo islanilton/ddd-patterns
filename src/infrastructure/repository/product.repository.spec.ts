dd82261d4cfb3a171ae3a53c95c0414913fef13c
Islanilton <islanilton.rodrigues@gmail.com>
2023-11-06 11:10:32 -0300

    chore: config swc to test with sequelize

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize-typescript'
+import { ProductModel } from '@/infrastructure/db/sequelize/model/product.model'
 
 describe('Product repository unit test', () => {
   let sequelize: Sequelize
@@ -10,6 +11,9 @@ describe('Product repository unit test', () => {
       logging: false,
       sync: { force: true },
     })
+
+    sequelize.addModels([ProductModel])
+    await sequelize.sync()
   })
 
   afterEach(async () => {
