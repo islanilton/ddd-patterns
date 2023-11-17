98d97ebbf89413a997d0a7eb9d2b46a674a5b9b3
Islanilton <islanilton.rodrigues@gmail.com>
2023-11-17 16:34:58 -0300

    feat: implemented register event method

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -2,35 +2,18 @@ import { EventDispatcher } from '@/domain/event/@shared/event-dispatcher'
 import { SendEmailWhenProductIsCreatedHandler } from '@/domain/event/product/handler/send-email-when-product-is-created.handler'
 
 describe('Domain events tests', () => {
-  it('should register a event handler', () => {
+  it('should register an event handler', () => {
     const eventDispatcher = new EventDispatcher()
     const eventHandler = new SendEmailWhenProductIsCreatedHandler()
 
     eventDispatcher.register('ProductCreatedEvent', eventHandler)
-    /* eslint-disable */
-    // Código onde você quer evitar a transformação do Prettier
-    // @ts-ignore
-    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent']).toBeDefined()
-    /* eslint-disable */
-    // Código onde você quer evitar a transformação do Prettier
-    // @ts-ignore
-    expect(eventDispatcher.getEventHandlers['ProductCreatedEvent'].length).toBe(1)
+
+    expect(eventDispatcher.getEventHandlers().ProductCreatedEvent).toBeDefined()
+    expect(eventDispatcher.getEventHandlers().ProductCreatedEvent.length).toBe(
+      1,
+    )
+    expect(
+      eventDispatcher.getEventHandlers().ProductCreatedEvent[0],
+    ).toMatchObject(eventHandler)
   })
-
-  it("should register an event handler", () => {
-    const eventDispatcher = new EventDispatcher();
-    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
-
-    eventDispatcher.register("ProductCreatedEvent", eventHandler);
-
-    expect(
-      eventDispatcher.getEventHandlers()["ProductCreatedEvent"]
-    ).toBeDefined();
-    expect(eventDispatcher.getEventHandlers()["ProductCreatedEvent"].length).toBe(
-      1
-    );
-    expect(
-      eventDispatcher.getEventHandlers()["ProductCreatedEvent"][0]
-    ).toMatchObject(eventHandler);
-  });
 })
