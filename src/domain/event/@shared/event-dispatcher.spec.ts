c54e17af13c017f32e98ab81f8559bf34df5f2b8
Islanilton <islanilton.rodrigues@gmail.com>
2023-11-20 11:21:22 -0300

    feat: implement unregister event and unregisterAll events

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -3,8 +3,9 @@ import { SendEmailWhenProductIsCreatedHandler } from '@/domain/event/product/han
 
 describe('Domain events tests', () => {
   it('should register an event handler', () => {
-    const eventDispatcher = new EventDispatcher()
-    const eventHandler = new SendEmailWhenProductIsCreatedHandler()
+    const eventDispatcher: EventDispatcher = new EventDispatcher()
+    const eventHandler: SendEmailWhenProductIsCreatedHandler =
+      new SendEmailWhenProductIsCreatedHandler()
 
     eventDispatcher.register('ProductCreatedEvent', eventHandler)
 
@@ -15,5 +16,39 @@ describe('Domain events tests', () => {
     expect(
       eventDispatcher.getEventHandlers().ProductCreatedEvent[0],
     ).toMatchObject(eventHandler)
+  })
+
+  it('should unregister an event handler', () => {
+    const eventDispatcher: EventDispatcher = new EventDispatcher()
+    const eventHandler: SendEmailWhenProductIsCreatedHandler =
+      new SendEmailWhenProductIsCreatedHandler()
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler)
+    expect(eventDispatcher.getEventHandlers().ProductCreatedEvent).toBeDefined()
+    eventDispatcher.unregister('ProductCreatedEvent', eventHandler)
+    expect(eventDispatcher.getEventHandlers().ProductCreatedEvent).toBeDefined()
+    expect(eventDispatcher.getEventHandlers().ProductCreatedEvent.length).toBe(
+      0,
+    )
+  })
+
+  it('should unregister all event handlers', () => {
+    const eventDispatcher: EventDispatcher = new EventDispatcher()
+    const eventHandler: SendEmailWhenProductIsCreatedHandler =
+      new SendEmailWhenProductIsCreatedHandler()
+
+    eventDispatcher.register('ProductCreatedEvent', eventHandler)
+    expect(eventDispatcher.getEventHandlers().ProductCreatedEvent).toBeDefined()
+    expect(eventDispatcher.getEventHandlers().ProductCreatedEvent.length).toBe(
+      1,
+    )
+    expect(
+      eventDispatcher.getEventHandlers().ProductCreatedEvent[0],
+    ).toMatchObject(eventHandler)
+
+    eventDispatcher.unregisterAll()
+    expect(
+      eventDispatcher.getEventHandlers().ProductCreatedEvent,
+    ).toBeUndefined()
   })
 })
